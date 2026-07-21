17c6a8d150012e176897b200f9cce086319aff2f
Shima Al Sharqi <Shimaalsharqi@gmail.com>
2026-07-21 11:14:29 +0400

    Add Payment Class

--- Sea-Trips-System/Sea-Trips-System/Models/Payment.cs
+++ Sea-Trips-System/Sea-Trips-System/Models/Payment.cs
@@ -1,6 +1,34 @@
-﻿namespace Sea_Trips_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sea_Trips_System.Models
 {
-    public class PaymentDTOs
-    {
+
+        [Table("Payment")]
+        public class Payment
+        {
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int PaymentId { get; set; }                      // system generated
+
+            [Required]
+            [ForeignKey("Appointment")]
+            public int AppointmentId { get; set; }                  //From List ,foreign key
+
+            [Range(0.0, double.MaxValue)]
+            [Column(TypeName = "decimal(18,2)")]
+            [Required]
+            public decimal AmountPaid { get; set; }               //Calculated
+
+            [Required]
+            public string PaymentMethod { get; set; }             //Defult Value [Cash,Card,BankTtansfer]
+
+            [Required]
+            [MaxLength(30)]
+            public string PaymentStatus { get; set; } = "Paid";    //Defult Value [Paid,PartiallyPaid, Refunded]
+
+            [Required]
+            public DateTime TransactionDate { get; set; }       //System Genrated
+        
     }
 }
