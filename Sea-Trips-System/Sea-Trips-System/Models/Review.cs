36a05e0aa5260f51a3675a1ef245b4a68f7fef64
Sheika <sheika02x2@gmail.com>
2026-07-21 11:06:00 +0400

    adding review work flow baranch and model.

--- Sea-Trips-System/Sea-Trips-System/Models/Review.cs
+++ Sea-Trips-System/Sea-Trips-System/Models/Review.cs
@@ -1,6 +1,20 @@
-﻿namespace Sea_Trips_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sea_Trips_System.Models
 {
-    public class ReviewDTOs
+    public class Review
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int ReviewId { get; set; } //system generated
+
+        [Required]
+        [Range(1, 5)]
+        public int Rating { get; set; } //user input ///
+
+        [MaxLength(1000)]
+        public string? Comment { get; set; }//user input //optional//
     }
 }
