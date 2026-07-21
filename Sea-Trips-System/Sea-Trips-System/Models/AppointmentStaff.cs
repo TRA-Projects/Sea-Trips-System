8aada521565c6a07af24f39c42d991d46209e6ba
Malakaka <malakalsenine@gmail.com>
2026-07-21 14:32:29 +0400

    adding appointmentStaffId

--- Sea-Trips-System/Sea-Trips-System/Models/AppointmentStaff.cs
+++ Sea-Trips-System/Sea-Trips-System/Models/AppointmentStaff.cs
@@ -1,9 +1,16 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Sea_Trips_System.Models
 {
     public class AppointmentStaff
     {
+
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+        public int appointmentStaffId { get; set; }    //system generated 
+
         [Required]
         public int appointmentId { get; set; }   // user input
 
