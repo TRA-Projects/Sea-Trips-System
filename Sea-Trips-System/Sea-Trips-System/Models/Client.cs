8596d3d205f73fad5355c4a0028b6d54df549dd6
widaad alhandasi <widaadalhandasi@gmail.com>
2026-07-21 12:11:39 +0400

    add client class

--- Sea-Trips-System/Sea-Trips-System/Models/Client.cs
+++ Sea-Trips-System/Sea-Trips-System/Models/Client.cs
@@ -1,6 +1,29 @@
-﻿namespace Sea_Trips_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sea_Trips_System.Models
 {
-    public class ClientControllers
+    [Table("User")]
+    public class Client
     {
+
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)] // Auto-generated
+            public int clientId { get; set; }    //system generated 
+
+            [Required]
+            [MaxLength(100)]
+            public string fullName { get; set; }   //user input
+
+            [Required]
+            [MaxLength(20)]
+            public string phone { get; set; }    //user input
+
+            [MaxLength(100)]
+            public string email { get; set; }  //user input
+
+            [Required]
+            public DateTime createdAt { get; set; }   ////system generated 
+
+        }
     }
-}
