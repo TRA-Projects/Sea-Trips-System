ec82c8bddd8e76d2e2ba08e76c9686a8971aefb8
moza-stack <moza.m@gmail.com>
2026-07-21 14:17:11 +0400

    update Trip

--- Sea-Trips-System/Sea-Trips-System/Models/TripType.cs
+++ Sea-Trips-System/Sea-Trips-System/Models/TripType.cs
@@ -7,19 +7,19 @@ namespace Sea_Trips_System.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]   //// Auto Increment (Identity)
-        public int TripTypeId { get; set; } // // Primary Key
+        public int tripTypeId { get; set; } // // Primary Key
 
         [Required(ErrorMessage = "Trip Type Name is required.")] //  // This field is required (cannot be null)
         [StringLength(100)]  ///// Maximum length is 100 characters
-        public string TypeName { get; set; }
+        public string typeName { get; set; }
 
         [Required(ErrorMessage = "Base Price is required.")]  ///// This field is required
         [Range(1, 999, ErrorMessage = "Base Price must be greater than 0.")] // // Value must be between 1 and 999
         [Column(TypeName = "decimal(18,2)")]  //  // Save as decimal(18,2) in SQL Server
-        public decimal BasePrice { get; set; }
+        public decimal basePrice { get; set; }
 
-        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")] // // Maximum length is 500 characters
-        public string Description { get; set; }
+        [StringLength(500, ErrorMessage = "Description cannot exceed 500 characters.")] // // Maximum length is 500 characters----
+        public string description { get; set; }
     }
 }
 
