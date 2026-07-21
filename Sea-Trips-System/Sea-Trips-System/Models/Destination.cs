585444dad78e0a9ce4081db7811543444ad4bed0
Sheika <sheika02x2@gmail.com>
2026-07-21 11:21:44 +0400

    add destination work flow

--- Sea-Trips-System/Sea-Trips-System/Models/Destination.cs
+++ Sea-Trips-System/Sea-Trips-System/Models/Destination.cs
@@ -1,6 +1,23 @@
-﻿namespace Sea_Trips_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Sea_Trips_System.Models
 {
-    public class DestinationDTOs
+    public class Destination
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Required]
+        public int DestinationId { get; set; }//system generated
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }//user input
+
+        [MaxLength(100)]
+        public string Coordinates { get; set; }//user input
+
+        [Required]
+        public TimeSpan EstimatedDuration { get; set; }//calculated
     }
 }
