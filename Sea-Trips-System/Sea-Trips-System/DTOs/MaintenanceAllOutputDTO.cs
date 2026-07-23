70eb315aa4e9f229d3adc86e4cfbb8ccf609bc59
widaad alhandasi <widaadalhandasi@gmail.com>
2026-07-23 14:16:10 +0400

    add client repo

---
+++ Sea-Trips-System/Sea-Trips-System/DTOs/MaintenanceAllOutputDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Sea_Trips_System.Models
+{
+    public class MaintenanceAllOutputDTO
+    {
+        public string description { get; set; }
+        public int maintenanceId { get; set; }
+
+        public DateTime startDate { get; set; }
+    }
+
+}
