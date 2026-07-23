6fe550a6ac05b78ac855912dc302c8fc6161d254
Sheika <sheika02x2@gmail.com>
2026-07-23 14:52:46 +0400

    add destination dto

--- Sea-Trips-System/Sea-Trips-System/DTOs/DestinationDTOs.cs
+++ Sea-Trips-System/Sea-Trips-System/DTOs/DestinationDTOs.cs
@@ -1,8 +1,27 @@
-﻿namespace Sea_Trips_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sea_Trips_System.Models
 {
     public class DestinationDTOs
     {
-        public string name { get; set; }
-        public string coordinates { get; set; }
+        public class DestinationInputDTOs
+        {
+            [Required(ErrorMessage = "Value should not be null.")]
+            [MaxLength(100, ErrorMessage = "name cannot exceed 100 characters.")]
+            public string name { get; set; }
+
+            [MaxLength(100, ErrorMessage = "coordinates cannot exceed 100 characters.")]
+            public string coordinates { get; set; }
+        }
+
+        public class DestinationOutputDTOs
+        {
+            public int destinationId { get; set; }
+            public string name { get; set; }
+            public string coordinates { get; set; }
+            public TimeSpan estimatedDuration { get; set; }
+
+        }
+
     }
 }
