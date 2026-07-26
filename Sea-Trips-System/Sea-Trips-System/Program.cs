77ed1a68ba23b79b7fda501d8f02355bfa27b5dc
Malakaka <malakalsenine@gmail.com>
2026-07-26 14:28:58 +0400

    Add Register AppointmentStaffRepo in DI Container.

--- Sea-Trips-System/Sea-Trips-System/Program.cs
+++ Sea-Trips-System/Sea-Trips-System/Program.cs
@@ -23,6 +23,8 @@ namespace Sea_Trips_System
             builder.Services.AddScoped<AppointmentRepo>();   // Register AppointmentRepo in DI Container. 
             builder.Services.AddScoped<ClientRepo>();         // Register ClientRepo in DI Container.
             builder.Services.AddScoped<StaffRepo>();          // Register StaffRepo in DI Container.
+            builder.Services.AddScoped<AppointmentStaffRepo>();  // Register AppointmentStaffRepo in DI Container. 
+
 
 
 
@@ -31,6 +33,8 @@ namespace Sea_Trips_System
             builder.Services.AddScoped<AppointmentService>();           // Register AppointmentService in DI Container.
             builder.Services.AddScoped<ClientService>();                // Register ClientService in DI Container.
             builder.Services.AddScoped<StaffService>();          // Register StaffRepo in DI Container.
+            builder.Services.AddScoped<AppointmentStaffServices>();  // Register AppointmentStaffServices in DI Container. 
+
 
 
 
