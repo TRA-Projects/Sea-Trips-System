09f2822b601b3f824d231c91cf7f3475e767fbe7
Malakaka <malakalsenine@gmail.com>
2026-07-26 09:30:06 +0400

    Add CreateAppointmentDto class

--- Sea-Trips-System/Sea-Trips-System/DTOs/StaffDTOs.cs
+++ Sea-Trips-System/Sea-Trips-System/DTOs/StaffDTOs.cs
@@ -2,5 +2,12 @@
 {
     public class StaffDTOs
     {
+        // ──*** Request DTO — Received when creating a new staff ***─-
+
+       public class CreateAppointmentDto
+        {
+
+        }
+
     }
 }
