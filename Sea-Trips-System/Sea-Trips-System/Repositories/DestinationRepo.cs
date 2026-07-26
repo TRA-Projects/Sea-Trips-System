70fc1fde8efec15843985d3cc3de419aea5d0847
Sheika <sheika02x2@gmail.com>
2026-07-26 10:24:25 +0400

    del

--- Sea-Trips-System/Sea-Trips-System/Repositories/DestinationRepo.cs
+++ Sea-Trips-System/Sea-Trips-System/Repositories/DestinationRepo.cs
@@ -2,62 +2,6 @@
 {
     public class DestinationRepo
     {
-        //cotext==>repo // Database Context
-        private SeaTripsContext context;
-
-
-        // Dependency Injection // Constructor
-        public DestinationRepo(SeaTripsContext _context)
-        {
-            context = _context;
-        }
-
-        // =====================================================
-        // GET ALL DESTINATIONS
-        // =====================================================
-
-        public List<Destination> GetAllDestinations()
-        {
-            return context.Destinations.ToList();
-        }
-
-        // =====================================================
-        // GET DESTINATION BY ID
-        // =====================================================
-        
-        public Destination GetDestinationById(int id)
-        {
-            return context.Destinations.FirstOrDefault(d => d.destinationId == id);
-        }
-
-        // =====================================================
-        // ADD DESTINATION
-        // =====================================================
-
-        public void Add(Destination destination)
-        {
-            //add new destination in dbset
-            context.Destinations.Add(destination);
-            context.SaveChanges();
-        }
-
-        // =====================================================
-        // UPDATE DESTINATION
-        // =====================================================
-
-        public void Update()
-        {
-            context.SaveChanges();
-        }
-
-        // =====================================================
-        // DELETE DESTINATION
-        // =====================================================
-
-        public void Delete(Destination destination)
-        {
-            context.Destinations.Remove(destination);
-            context.SaveChanges();
-        }
+       
     }
 }
