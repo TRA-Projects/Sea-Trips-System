9ee8814c37ebd7ae1fe46a11cde1746a0f8e464e
Malakaka <malakalsenine@gmail.com>
2026-07-26 15:10:13 +0400

    Add Constructor

--- Sea-Trips-System/Sea-Trips-System/Services/AppointmentStaffServices.cs
+++ Sea-Trips-System/Sea-Trips-System/Services/AppointmentStaffServices.cs
@@ -1,6 +1,19 @@
-﻿namespace Sea_Trips_System.Services
+﻿using Sea_Trips_System.Models;
+using Sea_Trips_System.Repositories;
+
+namespace Sea_Trips_System.Services
 {
     public class AppointmentStaffServices
     {
+
+        private  AppointmentStaffRepo appointmentStaffRepo ;
+        private AppointmentRepo appointmentRepo;
+        private StaffRepo staffRepo;
+
+       
+
+
+
+
     }
 }
