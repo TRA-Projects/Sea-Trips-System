9073a871724843717a553573428e5740f4424e73
Malakaka <malakalsenine@gmail.com>
2026-07-26 14:16:38 +0400

    Add AssignStaffDto

--- Sea-Trips-System/Sea-Trips-System/DTOs/AppointmentStaffDTOs.cs
+++ Sea-Trips-System/Sea-Trips-System/DTOs/AppointmentStaffDTOs.cs
@@ -1,6 +1,18 @@
-﻿namespace Sea_Trips_System.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sea_Trips_System.DTOs
 {
-    public class AppointmentStaffDTOs
+
+    public class AssignStaffDto
     {
+        [Required(ErrorMessage = "Appointment ID is required.")]
+        public int appointmentId { get; set; }
+
+        [Required(ErrorMessage = "Staff ID is required.")]
+        public int staffId { get; set; }
+
+        public string? assignedRole { get; set; }
+
+
     }
 }
