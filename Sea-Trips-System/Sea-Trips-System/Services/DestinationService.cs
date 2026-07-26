b180799b52db0db50626113321ffb20c7f9ca3fc
Sheika <sheika02x2@gmail.com>
2026-07-26 13:36:08 +0400

    Retrieves all destinations from the repository and maps them to Output DTOs.

--- Sea-Trips-System/Sea-Trips-System/Services/DestinationService.cs
+++ Sea-Trips-System/Sea-Trips-System/Services/DestinationService.cs
@@ -18,6 +18,17 @@
         // GET ALL DESTINATIONS
         // =====================================================
 
+        //Retrieves all destinations from the repository and maps them to Output DTOs.
+        public List<DestinationDTOs.DestinationOutputDTOs> GetAllDestinations()
+        {
+            return repo.GetAllDestinations().Select(destintion => new DestinationDTOs.DestinationOutputDTOs
+            {
+                destinationId = destintion.destinationId,
+                name = destintion.name,
+                coordinates = destintion.coordinates,
+                estimatedDuration = destintion.estimatedDuration
+            }).ToList();
+        }
 
         // =====================================================
         // GET DESTINATION BY ID
