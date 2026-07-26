573ce9667e3c386ffa07921a5313bb303ced5df1
Malakaka <malakalsenine@gmail.com>
2026-07-26 12:55:11 +0400

    Add Constructor StaffControllers

--- Sea-Trips-System/Sea-Trips-System/Controllers/StaffControllers.cs
+++ Sea-Trips-System/Sea-Trips-System/Controllers/StaffControllers.cs
@@ -1,6 +1,19 @@
-﻿namespace Sea_Trips_System.Models
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sea_Trips_System.Models
 {
     public class StaffControllers
     {
+        [ApiController]
+        [Route("api/[controller]")]
+        
+        public class StaffsController : ControllerBase
+        {
+            private StaffService staffService;
+            public StaffsController (StaffService _staffService)
+            {
+                staffService = _staffService;
+            }
+        }
     }
 }
