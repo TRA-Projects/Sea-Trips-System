c1c4583e9a68f48c8cc4d2b3a2a8644b7096fd6d
Shima Al Sharqi <Shimaalsharqi@gmail.com>
2026-07-26 15:04:54 +0400

    Add the Payment Repo

--- Sea-Trips-System/Sea-Trips-System/Repositories/PaymentRepo.cs
+++ Sea-Trips-System/Sea-Trips-System/Repositories/PaymentRepo.cs
@@ -1,6 +1,53 @@
-﻿namespace Sea_Trips_System.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sea_Trips_System.Models
 {
     public class PaymentRepo
     {
+        private SeaTripsContext context;
+
+        // Dependency Injection
+        public PaymentRepo(SeaTripsContext _context)
+        {
+            context = _context;
+        }
+
+        // View All Payments
+        public List<Payment> GetAllPayment()
+        {
+            return context.Payments.ToList();
+        }
+
+        // Make Payment
+        public void AddPayment(Payment payment)
+        {
+            context.Payments.Add(payment);
+            context.SaveChanges();
+        }
+
+        // Get Payment By Id
+        public Payment GetPaymentById(int paymentId)
+        {
+            return context.Payments
+                          .FirstOrDefault(p => p.paymentId == paymentId);
+        }
+
+        // Refund Payment
+        public bool RefundPayment(int paymentId)
+        {
+            Payment payment = context.Payments
+                                     .FirstOrDefault(p => p.paymentId == paymentId);
+
+            if (payment == null)
+            {
+                return false;
+            }
+
+            payment.paymentStatus = "Refunded";
+
+            context.SaveChanges();
+
+            return true;
+        }
     }
 }
