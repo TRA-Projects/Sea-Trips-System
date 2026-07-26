46f65065176be2b36c9af42b14b47ae885aa99bc
Malakaka <malakalsenine@gmail.com>
2026-07-26 14:17:14 +0400

    Add AppointmentStaffResponseDto Class

--- Sea-Trips-System/Sea-Trips-System/DTOs/AppointmentStaffDTOs.cs
+++ Sea-Trips-System/Sea-Trips-System/DTOs/AppointmentStaffDTOs.cs
@@ -15,4 +15,16 @@ namespace Sea_Trips_System.DTOs
 
 
     }
+
+
+
+    public class AppointmentStaffResponseDto
+    {
+        public int appointmentStaffId { get; set; }
+        public int appointmentId { get; set; }
+        public int staffId { get; set; }
+        public string? staffName { get; set; }
+        public string? staffRole { get; set; }
+        public string? assignedRole { get; set; }
+    }
 }
