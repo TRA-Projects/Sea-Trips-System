dcd602542ab6f3945b28c349b2c9b7d810cf347b
Sheika <sheika02x2@gmail.com>
2026-07-26 10:01:01 +0400

    add context

--- Sea-Trips-System/Sea-Trips-System/Repositories/DestinationRepo.cs
+++ Sea-Trips-System/Sea-Trips-System/Repositories/DestinationRepo.cs
@@ -2,6 +2,20 @@
 {
     public class DestinationRepo
     {
+        //cotext==>repo // Database Context
+        private SeaTripsContext context;
 
+
+        // Dependency Injection // Constructor
+        public DestinationRepo(SeaTripsContext _context)
+        {
+            context = _context;
+        }
+
+        //get all destinations from DB
+        public List<Destination> GetAllDestinations()
+        {
+            return context.Destinations.ToList();
+        }
     }
 }
