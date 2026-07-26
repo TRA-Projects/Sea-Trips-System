8226608ce2635487d2965f5109d851098fbdbb03
Malakaka <malakalsenine@gmail.com>
2026-07-26 15:43:01 +0400

    Add AppointmentStaffControllers : ControllerBase

--- Sea-Trips-System/Sea-Trips-System/Controllers/AppointmentStaffControllers.cs
+++ Sea-Trips-System/Sea-Trips-System/Controllers/AppointmentStaffControllers.cs
@@ -1,6 +1,22 @@
-﻿namespace Sea_Trips_System.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Sea_Trips_System.Services;
+
+namespace Sea_Trips_System.Controllers
 {
-    public class AppointmentStaffControllers
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AppointmentStaffControllers : ControllerBase
     {
+
+        private readonly AppointmentStaffService appointmentStaffService;
+
+        public AppointmentStaffsController (AppointmentStaffService _appointmentStaffService)
+        {
+            appointmentStaffService = _appointmentStaffService;
+        }
+
+
+
     }
 }
