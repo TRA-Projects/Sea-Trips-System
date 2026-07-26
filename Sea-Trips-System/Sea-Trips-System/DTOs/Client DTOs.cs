47f093f054363c173bba80d4e0ef2df44219d85d
widaad alhandasi <widaadalhandasi@gmail.com>
2026-07-26 15:43:39 +0400

    update client dto

--- Sea-Trips-System/Sea-Trips-System/DTOs/Client DTOs.cs
+++ Sea-Trips-System/Sea-Trips-System/DTOs/Client DTOs.cs
@@ -11,15 +11,15 @@ namespace Sea_Trips_System.DTOs
     {
         [Required(ErrorMessage = "Full name is required.")]
         [MaxLength(100, ErrorMessage = "Full name cannot exceed 100 characters.")]
-        public string fullName { get; set; } = string.Empty;
+        public string fullName { get; set; }
 
         [Required(ErrorMessage = "Phone number is required.")]
         [MaxLength(20, ErrorMessage = "Phone number cannot exceed 20 characters.")]
-        public string phone { get; set; } = string.Empty;
+        public string phone { get; set; } 
 
         [MaxLength(100, ErrorMessage = "Email cannot exceed 100 characters.")]
         [EmailAddress(ErrorMessage = "Invalid email format.")]
-        public string email { get; set; } = string.Empty;
+        public string email { get; set; } 
 
         [Required(ErrorMessage = "Password is required.")]
         [MinLength(6, ErrorMessage = "Password must be at least 6 characters.")]
