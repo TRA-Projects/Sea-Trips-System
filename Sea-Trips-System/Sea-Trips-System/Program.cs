ea36edb22fe8c7f9570c4d9f0295750c8085c99e
Malakaka <malakalsenine@gmail.com>
2026-07-26 09:50:05 +0400

    Add builder.Services.AddScoped<StaffRepo>();

--- Sea-Trips-System/Sea-Trips-System/Program.cs
+++ Sea-Trips-System/Sea-Trips-System/Program.cs
@@ -22,13 +22,14 @@ namespace Sea_Trips_System
             //Repo
             builder.Services.AddScoped<AppointmentRepo>();   // Register AppointmentRepo in DI Container. 
             builder.Services.AddScoped<ClientRepo>();         // Register ClientRepo in DI Container.
+            builder.Services.AddScoped<StaffRepo>();          // Register StaffRepo in DI Container.
 
 
 
 
             //service
-            builder.Services.AddScoped<ClientService>();                // Register ClientService in DI Container.
             builder.Services.AddScoped<AppointmentService>();           // Register AppointmentService in DI Container.
+            builder.Services.AddScoped<ClientService>();                // Register ClientService in DI Container.
 
 
 
