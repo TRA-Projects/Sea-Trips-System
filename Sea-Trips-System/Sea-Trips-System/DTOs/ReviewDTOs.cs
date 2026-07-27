b7b02e9b3cdccaf981de860da4e7df2027c36ca5
Sheika <sheika02x2@gmail.com>
2026-07-27 10:08:47 +0400

    Input DTO for receiving review data from the user/client

--- Sea-Trips-System/Sea-Trips-System/DTOs/ReviewDTOs.cs
+++ Sea-Trips-System/Sea-Trips-System/DTOs/ReviewDTOs.cs
@@ -1,8 +1,16 @@
-﻿namespace Sea_Trips_System.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sea_Trips_System.Models
 {
     public class ReviewDTOs
     {
-        public int rating { get; set; }
-        public string? comment { get; set; }
+        //Input DTO for receiving review data from the user/client
+        public class ReviewInputDTOs
+        {
+            [Required(ErrorMessage = "Rating is required.")]
+            public int rating { get; set; }
+            public string? comment { get; set; }
+            public int AppointmentId { get; set; }
+        }
     }
 }
