bd6d51c629398f337b07cf851a9b98433be5ed52
Sheika <sheika02x2@gmail.com>
2026-07-27 16:21:41 +0400

    // Requires authentication for all endpoints by default

--- Sea-Trips-System/Sea-Trips-System/Controllers/ReviewControllers.cs
+++ Sea-Trips-System/Sea-Trips-System/Controllers/ReviewControllers.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Sea_Trips_System.Models
 {
     // Mark class as an API Controller and set the base URL route to "Review"
     [ApiController]
     [Route("Review")]
-    public class ReviewControllers
+    [Authorize] // Requires authentication for all endpoints by default
+    public class ReviewControllers: ControllerBase
     {
     }
 }
