2e200005aa8da38c3c8b218148f17201d45367ce
Sheika <sheika02x2@gmail.com>
2026-07-27 16:21:00 +0400

    // Mark class as an API Controller and set the base URL route to "Review"

--- Sea-Trips-System/Sea-Trips-System/Controllers/ReviewControllers.cs
+++ Sea-Trips-System/Sea-Trips-System/Controllers/ReviewControllers.cs
@@ -1,5 +1,10 @@
-﻿namespace Sea_Trips_System.Models
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sea_Trips_System.Models
 {
+    // Mark class as an API Controller and set the base URL route to "Review"
+    [ApiController]
+    [Route("Review")]
     public class ReviewControllers
     {
     }
