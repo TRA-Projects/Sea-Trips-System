de3c7c1233d580f565945bc867a052234401c840
Sheika <sheika02x2@gmail.com>
2026-07-27 16:45:00 +0400

    // 3. Return HTTP 200 OK with a confirmation message that deletion succeeded

--- Sea-Trips-System/Sea-Trips-System/Controllers/ReviewControllers.cs
+++ Sea-Trips-System/Sea-Trips-System/Controllers/ReviewControllers.cs
@@ -153,6 +153,9 @@ namespace Sea_Trips_System.Models
             {
                 return NotFound(); // HTTP 404 Not Found
             }
+
+            // 3. Return HTTP 200 OK with a confirmation message that deletion succeeded
+            return Ok("Deleted successfully"); // HTTP 200 OK
         }
     }
 }
