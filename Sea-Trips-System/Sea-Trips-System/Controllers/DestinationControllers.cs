48879c34af8e975229e593b0e119b10838618cb9
Sheika <sheika02x2@gmail.com>
2026-07-27 08:33:49 +0400

    ss

--- Sea-Trips-System/Sea-Trips-System/Controllers/DestinationControllers.cs
+++ Sea-Trips-System/Sea-Trips-System/Controllers/DestinationControllers.cs
@@ -5,10 +5,13 @@ namespace Sea_Trips_System.Models
     public class DestinationControllers:ControllerBase
     {
         private DestinationService destinationService;
+
         // Dependency Injection
         public DestinationControllers(DestinationService _destinationService)
         {
-            destinationService = _destinationService;
+            destinationService = _destinationService;//
         }
+
+
     }
 }
