d44250d4332df3f5429218f2115ccc9043acbc16
Sheika <sheika02x2@gmail.com>
2026-07-27 15:57:24 +0400

    // 2. Dependency Injection: Injecting ReviewRepo instance

--- Sea-Trips-System/Sea-Trips-System/Services/ReviewService.cs
+++ Sea-Trips-System/Sea-Trips-System/Services/ReviewService.cs
@@ -5,5 +5,11 @@
         // 1. Declare the target Repository instance
         private ReviewRepo reviewRepo;
 
+        // 2. Dependency Injection: Injecting ReviewRepo instance
+        public ReviewService(ReviewRepo _reviewRepo)
+        {
+            reviewRepo = _reviewRepo;
+        }
+
     }
 }
