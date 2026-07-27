3d8a06295f5f4b2d5995982c2baa4b8a895fcdbc
Sheika <sheika02x2@gmail.com>
2026-07-27 15:30:44 +0400

    // Include Appointment & Destination to get full details

--- Sea-Trips-System/Sea-Trips-System/Repositories/ReviewRepo.cs
+++ Sea-Trips-System/Sea-Trips-System/Repositories/ReviewRepo.cs
@@ -1,4 +1,6 @@
-﻿namespace Sea_Trips_System.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sea_Trips_System.Models
 {
     public class ReviewRepo
     {
@@ -9,6 +11,19 @@
         public ReviewRepo(SeaTripsContext _context)
         {
             context = _context;
+        }
+
+
+        // =====================================================
+        // GET ALL REVIEWS
+        // =====================================================
+        public List<Review> GetAllReviews()
+        {
+            // Include Appointment & Destination to get full details
+            return context.Reviews
+                .Include(r => r.Appointment)
+                    .ThenInclude(a => a.Destination)
+                .ToList();
         }
     }
 }
